6b384655aa994bf04aa2455e8bd5b333bfebb367
Olaniyidavid <55438610+Olaniyidavid@users.noreply.github.com>
2023-04-25 13:38:43 +0100

    Update nigerianpropertypro.py

--- nigerianpropertypro.py
+++ nigerianpropertypro.py
@@ -4,20 +4,19 @@ import requests
 
 from bs4 import BeautifulSoup
 #SCRAPE ALL THE HOUSE DESCRIPTION ON NIGERIAN PROPERTY PRO AND SAVE INTO A FILE
-file = open("Test.txt","w",encoding = "utf-8")
-url = 'https://nigeriapropertycentre.com/for-rent/houses/lagos/showtype'
-for page in range(1,2):
-    data = requests.get(url + '?page=' + str(page))
-    soup = BeautifulSoup(data.text,"html.parser")
-    houses = soup.find_all('div',class_="wp-block-body")
-    for house in houses:
-        room = house.find('div',class_="wp-block-content clearfix text-xs-left text-sm-left")
-        roo = room.find('a',href=True)
-        NoOfRoom = roo.find('h4',class_="content-title").text
-        Loc = house.find('div',class_="wp-block-content clearfix text-xs-left text-sm-left")
-        Loca = Loc.find('address',class_="voffset-bottom-10")
-        Location = Loca.find('strong').text
-        r = (f"{NoOfRoom} {Location}\n")
-        file.write(r)
-    file.flush()
-    file.close
+propertydata = pd.DataFrame(columns=["Number of bedroom", "Location","Price"])
+data = requests.get('https://nigeriapropertycentre.com/for-rent/houses/lagos/showtype').text
+soup = BeautifulSoup(data,"html.parser")
+houses = soup.find_all('div',class_="wp-block-body")
+for house in houses:
+    room = house.find('div',class_="wp-block-content clearfix text-xs-left text-sm-left")
+    roo = room.find('a',href=True)
+    NoOfRoom = roo.find('h4',class_="content-title").text
+    Loc = house.find('div',class_="wp-block-content clearfix text-xs-left text-sm-left")
+    Loca = Loc.find('address',class_="voffset-bottom-10")
+    Location = Loca.find('strong').text
+    Pri = house.find('div',class_="wp-block-content clearfix text-xs-left text-sm-left")
+    Price = Pri.find('span',class_="pull-sm-left").text
+    moreinfo = house.find('a',href = True)
+   
+    propertydata = propertydata.append({"Number of bedroom":NoOfRoom, "Location":Location,"Price":Price}, ignore_index=True)
